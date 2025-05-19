1736689a28878496318b0e1cfa45a01f2b12791c
marek krncok <krncok36@gmail.com>
2025-05-19 21:24:44 +0200

    Create Team

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,12 +1,34 @@
 <template>
     <h1>Všetky tými:</h1>
 
- <ul v-if="!loading">
-  <li v-for="team in teams" :key="team.id">
-    <strong>Hráč 1:</strong> {{ team.player1.firstName }} {{ team.player1.lastName }}<br>
-    <strong>Hráč 2:</strong> {{ team.player2.firstName }} {{ team.player2.lastName }}
-  </li>
-</ul>
+    <div v-if="!loading">
+        <h2>Vytvoriť nový tím</h2>
+
+        <label for="player1">Hráč 1:</label>
+        <select id="player1" v-model="newTeam.player1Id">
+            <option disabled value="">-- Vyber hráča --</option>
+            <option v-for="player in players" :value="player.id">
+                {{ player.firstName }} {{ player.lastName }}
+            </option>
+        </select>
+
+        <label for="player2">Hráč 2:</label>
+        <select id="player2" v-model="newTeam.player2Id">
+            <option disabled value="">-- Vyber hráča --</option>
+            <option v-for="player in players" :value="player.id">
+                {{ player.firstName }} {{ player.lastName }}
+            </option>
+        </select>
+
+        <button @click="createTeam">Vytvoriť tím</button>
+
+        <ul>
+            <li v-for="team in teams" :key="team.id">
+                <strong>Hráč 1:</strong> {{ team.player1.firstName }} {{ team.player1.lastName }}<br>
+                <strong>Hráč 2:</strong> {{ team.player2.firstName }} {{ team.player2.lastName }}
+            </li>
+        </ul>
+    </div>
 
     <div v-else>... loading ...</div>
 </template>
@@ -20,12 +42,19 @@ export default {
     data() {
         return {
             teams: [],
-            loading: true
+            players: [],
+            loading: true,
+            newTeam: {
+                player1Id: '',
+                player2Id: ''
+            }
         }
     },
 
     created() {
         this.fetchTeams()
+        this.fetchPlayers()
+
     },
 
     methods: {
@@ -39,7 +68,51 @@ export default {
                     console.error('Chyba pri načítaní týmov', error)
                     this.loading = false
                 })
-        }
+        },
+        fetchPlayers() {
+            axios.get('/api/rest/players/')
+                .then((response) => {
+                    this.players = response.data
+                })
+                .catch((error) => {
+                    console.error('Chyba pri načítaní hráčov', error)
+                })
+        },
+        async createTeam() {
+            const payload = {
+                player1Id: this.newTeam.player1Id,
+                player2Id: this.newTeam.player2Id
+            };
+
+            try {
+                const res = await axios.post('/api/rest/teams/create', payload);
+                console.log('Tim: ' + res.data.id + ' bol úspešne vytvoreny.')
+                
+                // Resetovanie výberu hráčov
+                this.newTeam={player1Id:'',player2Id:''}
+
+                // Načítanie aktualizovaného zoznamu tímov
+                this.fetchTeams();
+            } catch (error) {
+                console.error('Chyba pri vytváraní tímu:', error);
+            }
+        },
+        // createTeam() {
+        //     const payload = {
+        //         player1Id: this.newTeam.player1Id,
+        //         player2Id: this.newTeam.player2Id
+        //     }
+
+        //     axios.post('/api/rest/teams/create', payload)
+        //         .then(() => {
+        //             this.newTeam.player1Id = ''
+        //             this.newTeam.player2Id = ''
+        //             this.fetchTeams() // refresh tímov po vytvorení
+        //         })
+        //         .catch((error) => {
+        //             console.error('Chyba pri vytváraní tímu:', error)
+        //         })
+        // }
     }
 }
 
@@ -59,5 +132,4 @@ li {
 li:not(:last-child) {
     border-bottom: 1px solid #cdcdcd;
 }
-
 </style>
